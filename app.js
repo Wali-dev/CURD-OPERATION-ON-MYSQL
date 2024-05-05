bafb90bd8d837989e087f77a86a319c15d49d928
Wali Cornell <cornelhassan780@gmail.com>
2024-05-05 18:49:12 +0530

    finalised

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const productRouter= require("./routes/productRoute");
 const logRouter = require("./routes/logRoute");
 
+app.get("/", (req, res)=>{
+    res.send("This is the Homepage")
+})
 
 // Middleware
 app.use(express.json());
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 9000;
 app.get("/", (req, res)=>{
     res.send("This is the Homepage")
 })
+
 app.listen(PORT, ()=>{
     logger.info(`The server is running on http://localhost:${PORT}`);
 })
